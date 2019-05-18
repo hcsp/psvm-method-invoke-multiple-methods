58cc2bb08feba8e2f1599c02a3670aa20c9c199d
Bo Zhang <bo@gradle.com>
2019-05-18 20:47:39 +0800

    Refine comments

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,17 +1,26 @@
 package com.github.hcsp;
 
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        // invoke method a() here
+        // 在这里调用方法a()
+    }
 
     public static void a() {
+        // invoke method b() here
+        // 在这里调用方法b()
         System.out.println("a");
     }
 
     public static void b() {
+        // invoke method c() here
+        // 在这里调用方法c()
         System.out.println("b");
     }
 
     public static void c() {
+        // invoke method d() here
+        // 在这里调用方法d()
         System.out.println("c");
     }
 
