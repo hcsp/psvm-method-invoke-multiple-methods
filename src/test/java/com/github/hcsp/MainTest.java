2e81a289db3933c576899756ebb96dbb0b391711
Bo Zhang <bo@gradle.com>
2019-06-09 10:51:19 +0800

    Fix newline issue on Windows

--- src/test/java/com/github/hcsp/MainTest.java
+++ src/test/java/com/github/hcsp/MainTest.java
@@ -1,6 +1,6 @@
 package com.github.hcsp;
 
-import static org.hamcrest.Matchers.containsStringIgnoringCase;
+import static org.hamcrest.Matchers.matchesRegex;
 
 import com.github.blindpirate.extensions.CaptureSystemOutput;
 import org.junit.jupiter.api.Test;
@@ -9,7 +9,7 @@ public class MainTest {
     @Test
     @CaptureSystemOutput
     public void importStringUtilsCorrectly(CaptureSystemOutput.OutputCapture capture) {
-        capture.expect(containsStringIgnoringCase("a\nb\nc\nd\n"));
+        capture.expect(matchesRegex("(?s).*a\\s+b\\s+c\\s+d\\s+.*"));
         Main.main(null);
     }
 }
