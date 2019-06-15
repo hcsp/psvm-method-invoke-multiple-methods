2ddf6dc39960226ed9c4168dc3b3b71c505fbf14
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:06 +0800

    Update repo

--- src/test/java/com/github/hcsp/MainTest.java
+++ src/test/java/com/github/hcsp/MainTest.java
@@ -1,15 +1,14 @@
 package com.github.hcsp;
 
-import static org.hamcrest.Matchers.matchesRegex;
-
 import com.github.blindpirate.extensions.CaptureSystemOutput;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
     @Test
     @CaptureSystemOutput
     public void importStringUtilsCorrectly(CaptureSystemOutput.OutputCapture capture) {
-        capture.expect(matchesRegex("(?s).*a\\s+b\\s+c\\s+d\\s+.*"));
+        capture.expect(Matchers.matchesRegex("(?s).*a\\s+b\\s+c\\s+d\\s+.*"));
         Main.main(null);
     }
 }
