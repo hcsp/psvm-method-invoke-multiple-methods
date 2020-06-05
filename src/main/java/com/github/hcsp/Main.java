4af0f023c60c00781c20134ba2c9de0d46aa90d7
ZEJIA LIU <53506531+ZEJIA-LIU@users.noreply.github.com>
2020-06-05 16:55:58 +0800

    嵌套调用多个方法 (#218)
    嵌套调用多个方法 (#218)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -2,26 +2,22 @@ package com.github.hcsp;
 
 public class Main {
     public static void main(String[] args) {
-        // invoke method a() here
-        // 在这里调用方法a()
+        a();
     }
 
     public static void a() {
         System.out.println("a");
-        // invoke method b() here
-        // 在这里调用方法b()
+        b();
     }
 
     public static void b() {
         System.out.println("b");
-        // invoke method c() here
-        // 在这里调用方法c()
+        c();
     }
 
     public static void c() {
         System.out.println("c");
-        // invoke method d() here
-        // 在这里调用方法d()
+        d();
     }
 
     public static void d() {
