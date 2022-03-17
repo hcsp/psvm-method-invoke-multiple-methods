97cb9a1be71c3248eef8c4965577560f616e8094
LiDianguang <101635266+LiDianguang@users.noreply.github.com>
2022-03-18 06:40:52 +0800

    Update Main.java

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -4,24 +4,28 @@ public class Main {
     public static void main(String[] args) {
         // invoke method a() here
         // 在这里调用方法a()
+        a();
     }
 
     public static void a() {
         System.out.println("a");
         // invoke method b() here
         // 在这里调用方法b()
+        b();
     }
 
     public static void b() {
         System.out.println("b");
         // invoke method c() here
         // 在这里调用方法c()
+        c();
     }
 
     public static void c() {
         System.out.println("c");
         // invoke method d() here
         // 在这里调用方法d()
+        d();
     }
 
     public static void d() {
